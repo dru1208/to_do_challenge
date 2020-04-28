037d47c39c660c92c9c6ba9153b47036e061e352
andrew hsieh <andrewhsieh1208@gmail.com>
2020-04-28 11:04:29 -0400

    [ah] form is toggled based on button

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,16 +1,27 @@
-import React from "react"
+import React, { useState } from "react"
 import ToDo from "./ToDo"
 import ToDosHeader from "./ToDosHeader"
 import ToDoForm from "./ToDoForm"
 import "./ToDos.css"
 
 const ToDoList = ({ toDos, listName }) => {
+  const [activeForm, setActiveForm] = useState(false)
+
+  const toggleForm = () => {
+    setActiveForm(prev => !prev)
+  }
+
   return (
     <div className="list-container">
       <ToDosHeader listName={listName} />
       { toDos.map((toDo, index) => <ToDo toDo={toDo} key={index} />)}
-      <button className="list-button">Add ToDo</button>
-      <ToDoForm addToDo={""} />
+      <button
+        className="list-button"
+        onClick={toggleForm}
+      >
+        Add ToDo
+      </button>
+      { activeForm ? <ToDoForm addToDo={""} /> : null }
     </div>
   )
 }
